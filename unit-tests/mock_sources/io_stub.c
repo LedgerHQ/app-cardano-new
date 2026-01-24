a4c0b8f3c47fbb3de6ebbad22e1cade4f6b51af8
Jan Mazak <jan.mazak@vacuumlabs.com>
2026-01-24 01:33:48 +0100

    remove useless mock

--- unit-tests/mock_sources/io_stub.c
+++ unit-tests/mock_sources/io_stub.c
@@ -14,18 +14,6 @@ __attribute__((weak)) void send_swo_and_reset(uint16_t swo) {
     io_send_sw(swo);
 }
 
-bool buffer_can_read(const void *buffer, size_t n) {
-    // Mock implementation - check if buffer has at least n bytes available
-    typedef struct {
-        const uint8_t *ptr;
-        size_t size;
-        size_t offset;
-    } buffer_t;
-
-    const buffer_t *buf = (const buffer_t *)buffer;
-    return (buf->size - buf->offset) >= n;
-}
-
 uintptr_t pic(uintptr_t linked_address) {
     return linked_address;
 }
