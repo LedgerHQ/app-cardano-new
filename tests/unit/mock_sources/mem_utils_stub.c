5b6c1dd8fdbd8dd1987586c06a0212101d7e01ba
Jan Mazak <jan.mazak@vacuumlabs.com>
2026-05-05 13:12:57 +0200

    clean up unit test mocks

--- tests/unit/mock_sources/mem_utils_stub.c
+++ tests/unit/mock_sources/mem_utils_stub.c
@@ -15,20 +15,6 @@ bool mem_utils_init(void *heap_start, size_t heap_size) {
     return true;
 }
 
-void *mem_utils_alloc(size_t size, bool permanent, const char *file, int line) {
-    (void) permanent;
-    (void) file;
-    (void) line;
-    if (size == 0) {
-        return NULL;
-    }
-    void *ptr = malloc(size);
-    if (ptr != NULL) {
-        memset(ptr, 0, size);
-    }
-    return ptr;
-}
-
 void *mem_utils_realloc(void *ptr, size_t size, const char *file, int line) {
     (void) file;
     (void) line;
@@ -52,20 +38,6 @@ void mem_utils_free_and_null(void **buffer, const char *file, int line) {
         free(*buffer);
         *buffer = NULL;
     }
-}
-
-char *mem_utils_strdup(const char *s, const char *file, int line) {
-    (void) file;
-    (void) line;
-    if (s == NULL) {
-        return NULL;
-    }
-    size_t len = strlen(s) + 1;
-    char *dst = malloc(len);
-    if (dst != NULL) {
-        memcpy(dst, s, len);
-    }
-    return dst;
 }
 
 bool mem_utils_calloc(void **buffer, uint16_t size, bool permanent, const char *file, int line) {
